f99e0d202114ec7c81cc15e5383ebcaebaf5d962
nath-hub <nathalietaffot@gmail.com>
2024-03-07 21:00:39 +0100

    init

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+  @include('element.head' ) 
+
+<body>
+    
+    @include('element.header' )
+
+
+</body>
+@include('element.footer' )
+
+</html>
---
+++ resources/views/element/slider.blade.php
@@ -0,0 +1,126 @@
+
+<main class="site-main">
+    <!--=========== Slider Section Start =========-->
+    <section class="tj-slider-section">
+        <div class="swiper hero-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide tj-slider-item">
+                    <div class="slider-bg-image" data-bg-image="assets/images/banner/ban1.jpg"></div>
+                    <div class="container">
+                        <div class="slider-content">
+                            <h1 class="slider-title">Tandis</h1>
+                            <h2 class="slider-sub-title"> <span>Elevage</span></h2>
+                            <div class="slider-text">
+                                <div class="desc">
+                                    <p>
+                                        Avec TANDIS les produits agricole a porter de mains.
+                                        Avoir access en permanance au poulet et au produits biologique ,contoles et a moindre prix, cela est desormais possible avec nous.
+                                    </p>
+                                </div>
+                                <div class="slider-button d-flex">
+                                    <a class="tj-transparent-btn btn" href="contact.html">
+                                       En savoir plus <i class="flaticon-right-arrow"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide tj-slider-item">
+                    <div class="slider-bg-image" data-bg-image="assets/images/banner/ban4.jpg"></div>
+                    <div class="container">
+                        <div class="slider-content">
+                            <h1 class="slider-title">Tandis Investissement</h1>
+                            <h2 class="slider-sub-title"> <span> Lévée de fond </span></h2>
+                            <div class="slider-text">
+                                <div class="desc">
+                                    <p>
+                                        Acheté vos actions facilement et a moindre coût
+                                    </p>
+                                </div>
+                                <div class="slider-button d-flex">
+                                    <a class="tj-transparent-btn btn" href="contact.html">
+                                        Investir <i class="flaticon-right-arrow"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide tj-slider-item">
+                    <div class="slider-bg-image" data-bg-image="assets/images/banner/ban6.jpg"></div>
+                    <div class="container">
+                        <div class="slider-content">
+                            <h1 class="slider-title">Harnessing The Wind</h1>
+                            <h2 class="slider-sub-title">And Sun <span>For Future</span></h2>
+                            <div class="slider-text">
+                                <div class="desc">
+                                    <p>
+                                        There are many variations of passages of Lorem Ipsum available, but the
+                                        majority have suffered form, passage.
+                                    </p>
+                                </div>
+                                <div class="slider-button d-flex">
+                                    <a class="tj-transparent-btn btn" href="contact.html">
+                                        Read More <i class="flaticon-right-arrow"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+        <div class="slider-sec-shape">
+            <img src="assets/images/shape/hero-shape.svg" alt="Shape" />
+        </div>
+        <div class="tj-service-item">
+            <ul class="dot-style">
+                <li>
+                    <div class="service-content">
+                        <div class="service-icon">
+                            <i class="flaticon-solar-panel"></i>
+                        </div>
+                        <div class="service-text">
+                            <h5 class="title">Utilisateurs</h5>
+                            <span class="sub-title">46 Personnes connectées</span>
+                        </div>
+                        <div class="item-shape">
+                            <img src="assets/images/shape/service-1.svg" alt="Shape" />
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="service-content">
+                        <div class="service-icon">
+                            <i class="flaticon-solar-energy"></i>
+                        </div>
+                        <div class="service-text">
+                            <h5 class="title">Objectif</h5>
+                            <span class="sub-title">6280000 frs</span>
+                        </div>
+                        <div class="item-shape">
+                            <img src="assets/images/shape/service-1.svg" alt="Shape" />
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="service-content">
+                        <div class="service-icon">
+                            <i class="flaticon-battery"></i>
+                        </div>
+                        <div class="service-text">
+                            <h5 class="title">Montant Investit</h5>
+                            <span class="sub-title">Total  530000 frs</span>
+                        </div>
+                        <div class="item-shape">
+                            <img src="assets/images/shape/service-1.svg" alt="Shape" />
+                        </div>
+                    </div>
+                </li>
+                
+            </ul>
+        </div>
+    </section>
+    <!--=========== Slider Section End =========-->
