375ab21a4155c268d75d6c33a9b6e09817cbdbda
nath-hub <nathalietaffot@gmail.com>
2024-03-07 21:06:04 +0100

    init

---
+++ storage/framework/views/c5a6e376ee1ddb7be40fe94636268991.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+  <?php echo $__env->make('element.head' , \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?> 
+
+<body>
+    
+    <?php echo $__env->make('element.header' , \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+</body>
+<?php echo $__env->make('element.footer' , \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+</html><?php /**PATH /home/ubix-group/sites/TandisApp/resources/views/about.blade.php ENDPATH**/ ?>
